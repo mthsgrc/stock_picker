5c94681a017a2e5b3810972fc7f185d62743581d
mthsgrc <matheus.dev.garcia@gmail.com>
2020-03-30 20:20:25 -0300

    first commit

---
+++ stock_picker.rb
@@ -0,0 +1,33 @@
+stock_array = [17,3,6,9,15,8,6,1,10] #0 8    length > 9
+
+def stock_picker(array)
+
+  best_day_array = []
+  best_profit = nil
+
+  array.each_with_index do |price, i|
+
+    for j in (i..array.length - 1)
+
+      profit = array[j] - price
+      p " #{array[j]} - #{price} ==== #{profit}"
+
+      if best_profit == nil || profit > best_profit
+        best_profit = profit
+        p "BEST PROFIT => #{best_profit}"
+        
+        best_day_array = [i, j]
+        p "**** best_day_array = [#{i}, #{j}] **** "
+
+        puts
+
+      end
+    end
+  end
+
+  p best_day_array
+
+end
+
+
+stock_picker(stock_array)
